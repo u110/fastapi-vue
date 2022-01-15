9dfe9c61e5ff079182a8a53554cb8d54df18679f
u110 <web.u.project@gmail.com>
2022-01-15 23:36:09 +0900

    frontend helloworld

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,10 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
 import Vue from 'vue'
+
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5000/';  // the FastAPI backend
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
