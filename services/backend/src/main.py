db76fa27ab30cd246bd019896c0c95ed57b3f732
u110 <web.u.project@gmail.com>
2022-01-16 23:18:55 +0900

    Models and Migrations

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,17 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  # NEW
 
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
+
 
 app = FastAPI()
 
-# NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# NEW
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
 
 @app.get("/")
