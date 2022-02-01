12d2e9c56babd9eb1236375a2bab081f34513724
u110 <web.u.project@gmail.com>
2022-02-02 00:53:56 +0900

    fix

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -17,16 +17,6 @@ export default {
 }
 </script>
 
-<script>
-// @ is an alias to /src
-import NavBar from '@/components/NavBar.vue'
-export default {
-  components: {
-    NavBar
-  }
-}
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
